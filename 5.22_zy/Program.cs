92d366dd69d02d6b2570b4d0eea8a44cb353e97b
孤独的蔬菜 <shuai19970217@163.com>
2017-05-22 18:43:40 +0800

    5.22作业

--- 5.22_zy/Program.cs
+++ 5.22_zy/Program.cs
@@ -10,6 +10,64 @@ namespace _5._22_zy
     {
         static void Main(string[] args)
         {
+            Robot robot1 = new Robot();
+
+            robot1.Name = "小度";
+
+            robot1.Eat(5);
+
+            Robot robot2 = new Robot();
+
+            robot2.Name = "小娜";
+
+            robot2.Eat(8);
+
+            Console.WriteLine("请选择机器人: 1：小度; 2： 小娜");
+
+            string str = Console.ReadLine();
+
+
+            Robot robot = null;
+
+            if (str == "1")
+
+            {
+
+                robot = robot1;
+
+            }
+
+            else if (str == "2")
+
+            {
+
+                robot = robot2;
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("你要找的机器人不存在!");
+
+                return;
+
+            }
+
+
+
+            robot.SayHello();
+
+            while (true)
+
+            {
+
+                string strChat = Console.ReadLine();
+
+                robot.Speak(strChat);
+
+            }
         }
     }
 }
---
+++ 5.22_zy/Robot.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._22_zy
+{
+    class Robot
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name
+
+        {
+            set;
+            get;
+        }
+
+        /// <summary>
+        /// 饥饿级别
+        /// </summary>
+        private int FullLevel  
+
+        {
+            set;
+            get;
+        }
+
+        
+        /// <summary>
+        /// 打印机器人的姓名
+        /// </summary>
+        public void SayHello()
+
+        {
+
+            Console.WriteLine("我叫:{0}", Name);
+
+        }
+
+        public void Eat(int foodCount)
+
+        {
+
+            if (FullLevel > 100)
+
+            {
+
+                return;
+
+            }
+
+            FullLevel += foodCount;
+
+        }
+
+        /// <summary>
+        /// 回答的问题
+        /// </summary>
+        /// <param name="str"></param>
+        public void Speak(string str)
+
+        {
+
+            if (FullLevel <= 0)
+
+            {
+
+                Console.WriteLine("饿死了,不说了!");
+
+                return;
+
+            }
+
+            if (str.Contains("名字"))
+
+            {
+
+                this.SayHello(); 
+
+            }
+
+            else if (str.Contains("女朋友"))
+
+            {
+
+                Console.WriteLine("女朋友是什么东西，能吃吗!");
+
+            }
+
+            else if (str.Contains("狗蛋"))
+
+            {
+
+                Console.WriteLine("狗蛋是什么东西!");
+
+            }
+        
+
+            else if (str.Contains("我帅"))
+
+            {
+
+                Console.WriteLine("好帅，我能做你的小弟吗？");
+
+            }
+            else if (str.Contains("不能"))
+
+            {
+
+                Console.WriteLine("我日!");
+
+            }
+            else
+
+            {
+
+                Console.WriteLine("不好意思,听不懂!");
+
+            }
+
+            FullLevel--;
+
+        }
+    }
+}
